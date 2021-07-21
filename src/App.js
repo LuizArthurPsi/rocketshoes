1e588a515b8a9dff68deb17d73f29c3b8ed76537
Luiz Arthur <luizarthurvm@gmail.com>
2021-07-21 19:43:52 -0300

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-    return <h1>Hello World</h1>;
+    return (
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    );
 }
 
 export default App;
