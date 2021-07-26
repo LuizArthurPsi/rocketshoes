54ffea7bd8d700f4a18203cc1f0b7d395a7a5380
Luiz Arthur <luizarthurvm@gmail.com>
2021-07-26 10:18:00 -0300

    Estilização da home
    Grid, lib polished

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
 import { ProductList } from './styles';
 
@@ -6,7 +7,100 @@ export default function Home() {
     return (
         <ProductList>
             <li>
-                <img src="" alt="" />
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-11-masculino/26/HZM-5208-026/HZM-5208-026_zoom1.jpg?ts=1620698712&ims=326x"
+                    alt="Tenis"
+                />
+                <strong>Tenis mto brabo</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span> ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-11-masculino/26/HZM-5208-026/HZM-5208-026_zoom1.jpg?ts=1620698712&ims=326x"
+                    alt="Tenis"
+                />
+                <strong>Tenis mto brabo</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span> ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-11-masculino/26/HZM-5208-026/HZM-5208-026_zoom1.jpg?ts=1620698712&ims=326x"
+                    alt="Tenis"
+                />
+                <strong>Tenis mto brabo</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span> ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-11-masculino/26/HZM-5208-026/HZM-5208-026_zoom1.jpg?ts=1620698712&ims=326x"
+                    alt="Tenis"
+                />
+                <strong>Tenis mto brabo</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span> ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-11-masculino/26/HZM-5208-026/HZM-5208-026_zoom1.jpg?ts=1620698712&ims=326x"
+                    alt="Tenis"
+                />
+                <strong>Tenis mto brabo</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span> ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-11-masculino/26/HZM-5208-026/HZM-5208-026_zoom1.jpg?ts=1620698712&ims=326x"
+                    alt="Tenis"
+                />
+                <strong>Tenis mto brabo</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span> ADICIONAR AO CARRINHO</span>
+                </button>
             </li>
         </ProductList>
     );
