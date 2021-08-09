efdd79786147e69a7116a8d9306a4c4cb40a5443
Luiz Arthur <luizarthurvm@gmail.com>
2021-08-09 13:29:45 -0300

    Redux Saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -26,11 +26,11 @@ class Home extends Component {
         this.setState({ products: data });
     }
 
-    handleAddProduct = product => {
+    handleAddProduct = id => {
         // eslint-disable-next-line react/prop-types
-        const { addToCart } = this.props;
+        const { addToCartRequest } = this.props;
 
-        addToCart(product);
+        addToCartRequest(id);
     };
 
     render() {
@@ -47,7 +47,7 @@ class Home extends Component {
 
                         <button
                             type="button"
-                            onClick={() => this.handleAddProduct(product)}
+                            onClick={() => this.handleAddProduct(product.id)}
                         >
                             <div>
                                 <MdAddShoppingCart size={16} color="#FFF" />{' '}
